61d12930c83c2dbdabe7bed597d56a251d344ea2
brian <brian.shann@gmail.com>
2018-03-07 07:30:06 +0000

    Fixed: ReferenceError: $ is not defined

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function search(query) {
       // The whole response has been received. Print out the result.
       resp.on('end', () => {
 
-      $ = cheerio.load(data, { ignoreWhitespace: true });
+     const $ = cheerio.load(data, { ignoreWhitespace: true });
 
         let synonyms = [];
 	let mostRelevantVal = 1
